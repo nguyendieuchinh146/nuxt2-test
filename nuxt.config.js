cb1956deafcb0a79fdfd9d0fd9d836d4b287e7a4
chinhnd <chinhnd146@gmail.com>
2023-06-25 11:18:29 +0700

    test 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/bootstrap.js',
     { src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client" },
     '~/plugins/script.js',
   ],
